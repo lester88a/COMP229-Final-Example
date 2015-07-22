a4e9e4fc4e1ab7f9b8f67055855084814fc75863
lester88a <lester88a@gmail.com>
2015-07-22 02:23:42 -0400

    add productModel and productTypeMOdel

--- App_Code/Models/ProductTypeModel.cs
+++ App_Code/Models/ProductTypeModel.cs
@@ -32,4 +32,50 @@ public class ProductTypeModel
             return e.Message;
         }
     }
+    //update product type by the id
+    public string UpdateProductType(int id, ProductType productType)
+    {
+        try
+        {
+            //get the database
+            FinalEntities db = new FinalEntities();
+            //get the table from db
+            ProductType pt = db.ProductTypes.Find(id);
+            //use the pt id to get the TypeDescreption from db
+            pt.TypeDescreption = productType.TypeDescreption;
+            //commit it save it
+            db.SaveChanges();
+            //return the got TypeDescreption for the lable which is resultLabel
+            return productType.TypeDescreption + "was successfully updated.";
+        }
+        catch (Exception e)
+        {
+
+            return e.Message;
+        }
+    }
+    //delete a productType by the id
+    public string DeleteProductType(int id)
+    {
+        try
+        {
+            //get the database
+            FinalEntities db = new FinalEntities();
+            //get the table from db
+            ProductType pt = db.ProductTypes.Find(id);
+            //attach() method Attaches a disconnected or "detached" entity to a new DataContext when original values are required for optimistic concurrency checks.
+            db.ProductTypes.Attach(pt);
+            //remove the product type by the id
+            db.ProductTypes.Remove(pt);
+            //save it
+            db.SaveChanges();
+            //return the got TypeDescreption for the lable which is resultLabel
+            return pt.TypeDescreption + "was successfully deleted.";
+        }
+        catch (Exception e)
+        {
+            
+            return e.Message;
+        }
+    }
 }
